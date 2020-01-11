4f1952d144e54bf8f05b2caeba4725f9c6376a85
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-01-11 12:35:44 -0500

    Added scroll on click animate functionality

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,20 @@ $(document).ready(function() {
     $('.sidenav').sidenav();
 });
 
+$("#jumboBtn").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".aboutTarget").offset().top
+    }, 2000);
+});
+
+$("#aboutBtn").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".aboutTarget").offset().top
+    }, 2000);
+});
+
+$("#portfolioBtn").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".portfolioTarget").offset().top
+    }, 2000);
+});
