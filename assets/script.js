2b7ebe55e2bb763880d4d1e3afd0de11909ff119
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-01-11 14:58:19 -0500

    Added sidenav button functionality, and bug fixes on mobile

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
     $('.sidenav').sidenav();
 });
 
+$(".sidenav-trigger").click(function() {
+    $(".sidenav").sidenav();
+})
+
+
+// Try removing the style: transform and set it to "none"
 $("#jumboBtn").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".aboutTarget").offset().top
@@ -19,4 +25,28 @@ $("#portfolioBtn").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".portfolioTarget").offset().top
     }, 2000);
+});
+
+$("#sideAboutBtn").click(function() {
+    $(".sidenav").removeAttr("style", "transform: translateX(0%)");
+    $(".sidenav").css("style", "transform: translateX(-105%)");
+    $(".sidenav-overlay").removeAttr("style", "opacity: 0");
+    $(".sidenav-overlay").removeAttr("style", "display: none");
+    $("body").removeAttr("style", "overflow: hidden");
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".aboutTarget").offset().top
+    }, 2000);
+    $(".sidenav-overlay").css("style", "opacity: 0");
+});
+
+$("#sidePortfolioBtn").click(function() {
+    $(".sidenav").removeAttr("style", "transform: translateX(0%)");
+    $(".sidenav").css("style", "transform: translateX(-105%)");
+    $(".sidenav-overlay").removeAttr("style", "opacity: 0");
+    $(".sidenav-overlay").removeAttr("style", "display: none");
+    $("body").removeAttr("style", "overflow: hidden");
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".portfolioTarget").offset().top
+    }, 2000);
+    $(".sidenav-overlay").css("style", "opacity: 0");
 });
